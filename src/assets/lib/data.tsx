bb8f9b77c5332b7707cfda1e7e2d61f1d723fb88
argolpour <argolpour@gmail.com>
2024-01-07 11:55:18 +0330

    version 1

---
+++ src/assets/lib/data.tsx
@@ -0,0 +1,621 @@
+import adobexdicon from "../../assets/icons/adobexdicon.svg";
+import canvaicon from "../../assets/icons/canvaicon.svg";
+import figmaicon from "../../assets/icons/figmaicon.svg";
+import htmlicon from "../../assets/icons/htmlicon.svg";
+import cssicon from "../../assets/icons/cssicon.svg";
+import javascripticon from "../../assets/icons/javascripticon.svg";
+import typescripticon from "../../assets/icons/typescripticon.svg";
+import reacticon from "../../assets/icons/reacticon.svg";
+import nextjsicon from "../../assets/icons/nextjsicon.svg";
+import mongodbicon from "../../assets/icons/mongoicon.svg";
+import redux from "../../assets/icons/redux.svg";
+import webrtc from "../../assets/icons/webrtc.svg";
+import sassscssicon from "../../assets/icons/sassicon.svg";
+import apiicon from "../../assets/icons/apiicon.svg";
+import axiosicon from "../../assets/icons/axiosicon.svg";
+import websocket from "../../assets/icons/websocket.svg";
+import linux from "../../assets/icons/linux.svg";
+import git from "../../assets/icons/git.svg";
+import docker from "../../assets/icons/docker.svg";
+import kubernetes from "../../assets/icons/kubernetes.svg";
+import ansible from "../../assets/icons/ansible.svg";
+import gitlab from "../../assets/icons/gitlab.svg";
+import pormetheus from "../../assets/icons/prometheus.svg";
+import nextjs from "../../assets/icons/next-js-.svg";
+import palphoneweb from "../../assets/img/palphoneweb.png";
+import backoffice from "../../assets/img/backoffice.png";
+import palphone from "../../assets/img/palphone.png";
+import profilepicture from "../img/index.jpg";
+import caricon from "../../assets/icons/car-icon.svg";
+import travelicon from "../../assets/icons/travel-icon.svg";
+import hardwareicon from "../../assets/icons/hardware-icon.svg";
+import nextjsiconwhite from "../../assets/icons/nextjsiconwhite.svg";
+import { GoHome, GoPerson, GoMail, GoStack, GoProject } from "react-icons/go";
+import { FiGithub, FiLink, FiLinkedin, FiMail } from "react-icons/fi";
+import Imprint from "../../components/Imprint";
+import Privacy from "../../components/Privacy";
+
+export const headerIntroData = {
+  title: {
+    de: "Hi, ich bin Alireza",
+    en: "Hi, I'm Alireza Golpour",
+  },
+  subtitle: "Frontend Developer & DevOps Engineer ",
+  description: {
+    de: "Ein erfahrener Front-End-Entwickler mit 2 Jahren Erfahrung im Designen und Entwickeln benutzerfreundlicher, responsiver Websites und Webanwendungen. Ich beherrsche HTML, CSS, JavaScript sowie moderne Front-End-Frameworks wie React und Containerisierungstools wie Docker. Darüber hinaus habe ich Erfahrung in der Programmierung von Bash-Skripten zur Automatisierung von Aufgaben sowie im Erlernen von CI/CD.",
+    en: "A skilled Front-End Developer with 2 years of experience designing and developing user-friendly, responsive websites and web applications. Proficient in HTML, CSS, JavaScript, and modern front-end frameworks such as React and containerization tools like docker and orchestraion like kubernetes and experienced in programming in bash script to automating tasks and in learning (CI/CD)"
+  },
+  buttons: [
+    {
+      name: "Contact",
+      label: {
+        de: "Kontaktiere mich",
+        en: "Contact me",
+      },
+      icon: FiMail,
+      color: "main-btn",
+    },
+    {
+      name: "Projects",
+      label: {
+        de: "Meine Projekte",
+        en: "My Projects",
+      },
+      icon: FiGithub,
+      color: "secondary-btn",
+    },
+  ],
+  profilepicture: profilepicture,
+} as const;
+
+export const projectsData = [
+  {
+    title: "palphone Web",
+    description:
+      "Palphone ist ein soziales Medium für anonyme Kommunikation, das es uns ermöglicht, anonym Anrufe zu tätigen.",
+    description_EN:
+      "Palphone is an anonymously social media that Let’s us have a call anonymously.",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "REST Api", icon: apiicon },
+      { name: "React", icon: reacticon },
+      { name: "Figma", icon: figmaicon },
+      { name: "Web Socket", icon: websocket },
+      { name: "redux", icon: redux },
+      { name: "mediaSoup", icon: webrtc },
+      { name: "Typescript", icon: typescripticon }
+     
+    ],
+    image: palphoneweb,
+    deploymenturl: "https://stg-web.palphone.com",
+    githuburl: "https://github.com/argolpour/palphonewebt",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#70B9BE",
+    },
+  },
+  {
+    title: "Palphone Website",
+    description:
+      "Diese Website ist eine mehrsprachige Website, die alle Informationen über die Palphone-App bereitstellt.",
+    description_EN:
+      " this website is a multi languages website which provides all information about Palphone App",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "Axios", icon: axiosicon },
+      { name: "Figma", icon: figmaicon },
+      { name: "next js", icon: nextjs },
+    ],
+    image: palphone,
+    deploymenturl: "https://palphone.com",
+    githuburl:
+      "https://github.com/argolpour/palphone-v2",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#FFD5BD",
+    },
+  },
+  {
+    title: "palphone Backoffice",
+    description:
+      "Eine optimierte App zur Bereitstellung von CRUD-Operationen für die Sperrung von Benutzern, das Management von Benutzerzugriffsrichtlinien, das Benutzermanagement und zur Bereitstellung verschiedener Berichte und Diagramme, um detaillierte Informationen gemäß den Geschäftsanforderungen zu erhalten.",
+    description_EN:
+      "an optimized app to use to Providing crud operation for User suspension, user access policy management, user management and provide different reports and charts to get detailed information based on business demands.",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "React", icon: reacticon },
+      { name: "Axios", icon: axiosicon },   
+      { name: "Figma", icon: figmaicon },
+      { name: "Typescript", icon: typescripticon }
+    ],
+    image: backoffice,
+    deploymenturl: "https://bo.palphone.com/",
+    githuburl: "backoffice",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#E3964A",
+    },
+  },
+  
+] as const;
+
+export const liveTickerData = {
+  content: {
+    de: "Weitere Projekte auf Github",
+    en: "More Projects on Github",
+  },
+} as const;
+
+export const skillsDataWeb = [
+  {
+    skillsTitle: "WebDevelopment",
+    skills: [
+      {
+        title: "HTML",
+        hash: "#html",
+        icon: htmlicon,
+        color: "#F1662A",
+      },
+      {
+        title: "CSS",
+        hash: "#CSS",
+        icon: cssicon,
+        color: "#1572B6",
+      },
+      {
+        title: "JavaScript",
+        hash: "#JavaScript",
+        icon: javascripticon,
+        color: "#F7DF1E",
+      },
+      {
+        title: "TypeScript",
+        hash: "#TypeScript",
+        icon: typescripticon,
+        color: "#007ACC",
+      },
+      {
+        title: "React",
+        hash: "#React",
+        icon: reacticon,
+        color: "#61DAFB",
+      },
+      {
+        title: "Next.js",
+        hash: "#Next.js",
+        icon: [nextjsicon, nextjsiconwhite],
+        color: ["#000000", "#FFFFFF"],
+      },
+      
+      // {
+      //   title: "Tailwind",
+      //   hash: "#Tailwind",
+      //   icon: tailwindcssicon,
+      //   color: "#38B2AC",
+      // },
+      {
+        title: "MongoDB",
+        hash: "#MongoDB",
+        icon: mongodbicon,
+        color: "#449C45",
+      },
+          {
+        title: "SASS/SCSS",
+        hash: "#SASS/SCSS",
+        icon: sassscssicon,
+        color: "#CC6699",
+      },
+    ],
+  },
+] as const;
+
+export const skillsDataDesign = [
+  {
+    skillsTitle: "Design",
+    skills: [
+      { title: "Figma", hash: "#Figma", icon: figmaicon, color: "#F24E1E" },
+      {
+        title: "Adobe XD",
+        hash: "#Adobe XD",
+        icon: adobexdicon,
+        color: "#FF61F6",
+      },
+      { title: "Canva", hash: "#Canva", icon: canvaicon, color: "#00C4CC" },
+    ],
+  },
+] as const;
+
+export const skillsDataCMS = [
+  {
+    skillsTitle: "Devops",
+    skills: [
+      {
+        title: "linux",
+        hash: "#Linux",
+        icon: linux,
+        color: "#21759B",
+      },
+      {
+        title: "Git",
+        hash: "#Git",
+        icon: git,
+        color: "#7AB55C",
+      },
+      {
+        title: "Docker",
+        hash: "#Docker",
+        icon: docker,
+        color: "#4353FF",
+      },
+       {
+        title: "Kubernetes",
+        hash: "#Kubernetes",
+        icon: kubernetes,
+        color: "#4353FF",
+      },
+        {
+        title: "Ansible",
+        hash: "#Ansible",
+        icon: ansible,
+        color: "#4353FF",
+      },
+         {
+        title: "Gitlab",
+        hash: "#Gitlab",
+        icon: gitlab,
+        color: "#4353FF",
+      },
+          {
+        title: "Pormetheus",
+        hash: "#Pormetheus",
+        icon: pormetheus,
+        color: "#4353FF",
+      }
+    ],
+  },
+] as const;
+
+export const navLinks = [
+  { de: "Home", en: "Home", hash: "#home", icon: GoHome },
+  { de: "Skills", en: "Skills", hash: "#skills", icon: GoStack },
+  { de: "Projekte", en: "Projects", hash: "#projects", icon: GoProject },
+  { de: "Über mich", en: "About me", hash: "#about-me", icon: GoPerson },
+  { de: "Kontakt", en: "Contact", hash: "#contact", icon: GoMail },
+] as const;
+
+export const FooterLinks = [
+  { de: "Impressum", en: "Imprint", hash: "#imprint", data: <Imprint /> },
+  { de: "Datenschutz", en: "Privacy", hash: "#privacy", data: <Privacy /> },
+] as const;
+
+export const sideBarRightMail = {
+  link: "mailto:myemail@com",
+  text: "myemail@com",
+} as const;
+
+export const sideBarLeftSocials = [
+  {
+    link: "https://www.linkedin.com/in/alireza-golpour-07a206176/",
+    icon: FiLinkedin,
+    altimgname: "linkedin",
+  },
+  {
+    link: "https://github.com/argolpour",
+    icon: FiGithub,
+    altimgname: "github",
+  },
+  {
+    link: "argolpour@gmail.com",
+    icon: FiMail,
+    altimgname: "mail",
+  },
+] as const;
+
+export const quotesData = [
+  {
+    de: '"Es ist überhaupt nicht wichtig, es beim ersten Mal richtig zu machen. Es ist entscheidend, es beim letzten Mal richtig zu machen."',
+    en: `"It's not at all important to get it right the first time. It's vitally important to get it right the last time."`,
+    author: "Andrew Hunt",
+  },
+  {
+    de: '"Das Web ist wie eine Leinwand und der Code ist die Farbe. Erschaffe dein Meisterwerk."',
+    en: `"The web is like a canvas, and code is the paint. Create your masterpiece."`,
+  },
+] as const;
+
+export const aboutMeData = {
+  title: "Über mich",
+  title_EN: "About me",
+  description: "Ein paar Codeschnippsel über mich",
+  description_EN: "A few code snippets about me",
+  paragraphs_DE: [
+    {
+      title: "Die Schaltkreise meines Herzens",
+      description:
+        "Wenn ich nicht in der digitalen Welt unterwegs bin, durchstreife ich die faszinierende Welt der Technik und Hardware. Schaltkreise und Lötstellen sind für mich wie Puzzleteile eines aufregenden Abenteuers.",
+      icon: hardwareicon,
+    },
+    {
+      title: "Auf der Überholspur des Lebens",
+      description:
+        "Neben dem Coden befinde ich mich gerne auf der Überholspur – im wahrsten Sinne des Wortes. Autos sind meine Leidenschaft, und ich genieße es, mit PS-starken Maschinen auf unbekannten Straßen unterwegs zu sein.",
+      icon: caricon,
+    },
+    {
+      title: "Entdeckungsfreude als Lebensmotto",
+      description:
+        "Meine Reise als Webentwickler ist nur ein Teil meines Lebenswegs. Ich lebe nach dem Motto, dass das Abenteuer erst beginnt, wenn man das Bekannte hinter sich lässt. Das Entdecken neuer Orte ist meine Form der kreativen Inspiration.",
+      icon: travelicon,
+    },
+  ],
+  paragraphs_EN: [
+    {
+      title: "The Circuits of My Heart",
+      description:
+        "When I'm not navigating the digital world, I explore the fascinating realm of technology and hardware. Circuits and solder joints are like puzzle pieces to me in an exciting adventure.",
+      icon: hardwareicon,
+    },
+    {
+      title: "On the Fast Lane of Life",
+      description:
+        "Besides coding, I like to be on the fast lane - in the truest sense of the word. Cars are my passion, and I enjoy being on unknown roads with powerful machines.",
+      icon: caricon,
+    },
+    {
+      title: "The Joy of Discovery as a Life Motto",
+      description:
+        "My journey as a web developer is only a part of my life path. I live by the motto that the adventure only begins when you leave the familiar behind. Discovering new places and cultures is my form of creative inspiration.",
+      icon: travelicon,
+    },
+  ],
+};
+
+export const contactData = {
+  title: {
+    de: "Kontakt",
+    en: "Contact",
+  },
+  description: {
+    de: "Schreib mir eine Nachricht und ich melde mich bei dir.",
+    en: "Write me a message and I will get back to you.",
+  },
+  inputfields: [
+    {
+      name: "name",
+      placeholder: {
+        de: "Dein Name",
+        en: "Your Name",
+      },
+      type: "text",
+      validation: {
+        de: "Bitte gebe deinen Namen ein",
+        en: "Please fill in your name",
+      },
+      pattern: "{2}",
+    },
+    {
+      name: "email",
+      placeholder: {
+        de: "Deine E-Mail Adresse",
+        en: "Your E-Mail",
+      },
+      type: "email",
+      validation: {
+        de: "Bitte gebe deine Email ein",
+        en: "Please fill in your email",
+      },
+      pattern: "[@]{4}",
+    },
+    {
+      name: "subject",
+      placeholder: {
+        de: "Deine Betreff",
+        en: "Your Subject",
+      },
+      type: "text",
+      validation: {
+        de: "Bitte gebe einen Betreff ein",
+        en: "Please fill in your subject",
+      },
+      pattern: "{10}",
+    },
+  ],
+  textarea: {
+    placeholder: {
+      de: "Deine Nachricht",
+      en: "Your Message",
+    },
+    name: "message",
+    rows: 10,
+    validation: {
+      de: "Bitte gebe deine Nachricht ein",
+      en: "Please fill in your message",
+    },
+    pattern: "{10}",
+  },
+  button: {
+    value: {
+      de: "Absenden",
+      en: "Send",
+    },
+  },
+  icon: FiMail,
+  iconcolor: "#FFFFFF",
+  colors: {
+    main: "main-btn",
+    second: "secondary-btn",
+    icon: "white",
+  },
+  privacyOptIn: {
+    checkbox: {
+      de: "Ich stimme zu, dass Alpay meine personenbezogenen Daten (Name und E-Mail-Adresse) verwenden darf, um mit mir Kontakt aufzunehmen.",
+      en: "I agree that Alpay may use my personal data (name and e-mail address) to contact me.",
+    },
+    description: {
+      de: "Durch Übermittlung dieser Anfrage bestätigen Sie, dass Sie die Datenschutzerklärung gelesen haben",
+      en: "By submitting this request, you acknowledge that you have read the Private Policy",
+    },
+  },
+} as const;
+
+export const toastMessages = {
+  loadingProject: {
+    de: "🦄 Die Live Demo wird gleich geöffnet. Server werden gestartet...",
+    en: "🦄 The live demo will open shortly. Starting servers...",
+  },
+  successEmailSent: {
+    de: "🦄 Vielen Dank für deine Email. Ich werde mich schnellstmöglich bei dir melden",
+    en: "🦄 Thank you for your email. I will get back to you as soon as possible",
+  },
+  failedEmailSent: {
+    de: "🦄 Leider hat der Versand deiner Email nicht geklappt. Bitte versuche es später noch einmal",
+    en: "🦄 Unfortunately the sending of your email did not work. Please try again later",
+  },
+  failedValidationName: {
+    de: "Bitte gebe deinen Namen ein",
+    en: "Please fill in your name",
+  },
+} as const;
+
+export const buttonLabels = {
+  language: {
+    de: "DE",
+    en: "EN",
+  },
+} as const;
+
+export const directionStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginRight: "4rem",
+  },
+  "outer-left-to-inner-right": {
+    marginLeft: "4rem",
+    transform: "scaleX(-1)",
+  },
+  "inner-right-to-middle": {
+    width: "100%",
+    transform: "scaleY(1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  "inner-left-to-middle": {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  "middle-to-inner-right": {
+    width: "100%",
+    transform: "scale(1,-1)",
+  },
+  "middle-to-inner-left": {
+    width: "100%",
+    transform: "scale(-1,-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  middle: {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
+
+export const heightStyles: Record<string, { heights: [string, string] }> = {
+  small: {
+    heights: ["25rem", "15rem"],
+  },
+  middle: {
+    heights: ["35rem", "25rem"],
+  },
+  large: {
+    heights: ["45rem", "35rem"],
+  },
+  extraLarge: {
+    heights: ["55rem", "45rem"],
+  },
+};
+
+export const spaceStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "outer-left-to-inner-right": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "inner-right-to-middle": {
+    marginTop: "-20rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "inner-left-to-middle": {
+    marginTop: "-10rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "middle-to-inner-right": {
+    width: "75%",
+  },
+  "middle-to-inner-left": {
+    marginTop: "-2.5rem",
+    width: "50%",
+  },
+  middle: {
+    marginTop: "-2.5rem",
+    width: "0%",
+    display: "none",
+  },
+};
+
+export const widthStyles: Record<string, { widths: [string, string] }> = {
+  "outer-right-to-inner-left": {
+    widths: ["74.45%", "74.45%"],
+  },
+  "outer-left-to-inner-right": {
+    widths: ["75%", "75%"],
+  },
+  "inner-right-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "inner-left-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "middle-to-inner-right": {
+    widths: ["33.4%", "50.03%"],
+  },
+  "middle-to-inner-left": {
+    widths: ["50.1%", "49%"],
+  },
+  middle: {
+    widths: ["0%", "0%"],
+  },
+};
